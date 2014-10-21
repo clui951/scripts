cf80ef7103a1e1096345d4368ffd68140100a62f
Calvin Lui <clui951@berkeley.edu>
2014-10-21 11:09:49 -0700

    hive_users.sh comments and LIMITED_TEST option

--- hive_users.sh
+++ hive_users.sh
@@ -2,12 +2,20 @@
 
 printf "\n"
 
+# LIMITED_TEST will only test hives known to previously work
+# 1 to test limited, 0 to test all hives
+LIMITED_TEST=1
+
 for serv_numb in {1..28} ; do
-	if [ "$serv_numb" == "6" -o "$serv_numb" == "7" -o "$serv_numb" == "9" -o "$serv_numb" == "14" -o "$serv_numb" == "17" -o "$serv_numb" == "20" -o "$serv_numb" == "23" -o "$serv_numb" == "25" ]; then
+
+	# check whether to test all and if known invalid server
+	if [  \( "$LIMITED_TEST" == "1" \) -a \( "$serv_numb" == "6" -o "$serv_numb" == "7" -o "$serv_numb" == "9" -o "$serv_numb" == "14" -o "$serv_numb" == "17" -o "$serv_numb" == "20" -o "$serv_numb" == "23" -o "$serv_numb" == "25" \) ]; then
 		echo "hive $serv_numb is not meant to be connected. access denied by default"
 		continue
 	fi
 
+	# gather number of users on specific hive
+	# if 0 users, check if network is reachable
 	NUMB="$(ssh cs61c-dv@hive$serv_numb.cs.berkeley.edu who | wc -l)" 
 	if [[ "$NUMB" -eq 0 ]]; then
 		echo "hive" $serv_numb "has" $NUMB "users. confirm if network reachable"
@@ -15,6 +23,7 @@ for serv_numb in {1..28} ; do
 	else 
 		echo "hive" $serv_numb "has" $NUMB "users"
 	fi
+
 done
 
 printf "\n"
